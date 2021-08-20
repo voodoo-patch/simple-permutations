09936abe5db8908ce0986908755bfd14e0f64dce
Samuele Pecetto <samuele.pec@gmail.com>
2021-08-20 17:38:50 +0200

    removed js from repo

--- main.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var simple_permutation_1 = require("./simple-permutation");
-var dict = [1, 2, 3, 4];
-simple_permutation_1.SimplePermutation.print(dict);
